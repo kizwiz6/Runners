8dd63dc2693a3c085dae8ae49d2694f0214eb9d5
Kieran Emery <kizwiz@hotmail.co.uk>
2023-11-05 21:43:53 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Runners
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Run> runs = new List<Run>();
+
+            if (int.TryParse(Console.ReadLine(), out int inputsCount))
+            {
+                for (int i = 0; i < inputsCount; i++)
+                {
+                    string line = Console.ReadLine();
+                    string[] lineParams = line.Split(' ');
+
+                    if (lineParams.Length == 3)
+                    {
+                        string personName = lineParams[0];
+                        string familyName = lineParams[1];
+
+                        if (double.TryParse(lineParams[2], out double runTime))
+                        {
+                            Run obj = new Run(personName, familyName, runTime);
+                            runs.Add(obj);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid run time format: " + lineParams[2]);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input: " + line);
+                    }
+                }
+
+                runs.Sort();
+
+                foreach (Run run in runs)
+                {
+                    Console.WriteLine($"{run.PersonName} {run.PersonFamilyName} {run.Time}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for the number of runs.");
+            }
+        }
+    }
+}
---
+++ Run.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Runners
+{
+    public class Run : IComparable<Run>
+    {
+        public string PersonName { get; }
+        public string PersonFamilyName { get; }
+        public double Time { get; }
+
+        public Run(string personName, string personFamilyName, double time)
+        {
+            PersonName = personName;
+            PersonFamilyName = personFamilyName;
+            Time = time;
+        }
+
+        public int CompareTo(Run other)
+        {
+            return Time.CompareTo(other.Time);
+        }
+    }
+}
